ab1b42f9a953d622e4392dc2776b4c4655911f1e
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-03-23 11:31:33 +0100

    added image to the card and more json

--- CardBase.gd
+++ CardBase.gd
@@ -5,9 +5,11 @@ var cards := preload("res://card-data.json").data
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for card in cards:
-		print(card.title)
+		print(card.keywords[0])
 		$MarginContainer/VBoxContainer/Label.text = card.description
 	
+	var cardSize = size
+	$Background.scale *= cardSize / $Background.texture.get_size()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
