af92e9fba8658a77960c44bfb0ab025e1729ee65
Erik Jan de Wit <erikjan.dewit@gmail.com>
2024-03-23 10:43:11 +0100

    added json

--- CardBase.gd
+++ CardBase.gd
@@ -1,9 +1,13 @@
 extends MarginContainer
 
+var cards := preload("res://cards-data.tres").data
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MarginContainer/VBoxContainer/Label.text = "Hello"
+	for card in cards:
+		print(card.title)
+		$MarginContainer/VBoxContainer/Label.text = card.description
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
